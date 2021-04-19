33bb445b2dbcce4f0e9a1039e9fcbe39e212e65a
GitcrackerUK <pfsiwek@gmail.com>
2021-04-19 17:16:50 +0100

    recreation directory

---
+++ src/components/commentBox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CommentBox extends Component {
+    render() {
+        return (
+            <div>
+             <h1>Comment Box</h1>
+            </div>
+        )
+    }
+}
