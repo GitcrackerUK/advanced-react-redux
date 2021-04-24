e0fed1d13d9bac0dabf4aa646dea21c54bdd6879
pawel.siwek <pawel.siwek@filament.uk.com>
2021-04-24 22:31:16 +0100

    added beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,20 +1,23 @@
-
 import CommentBox from '../commentBox';
 import CommentList from '../commentList';
-import { shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import App from '../App';
 
-// it() it function is a global function so doesn't need to be imported 
-  
+// it() it function is a global function so doesn't need to be imported
+
 // it('string to explain actual use of test', 'function which will be execute to test')
+let wrapper;
+beforeEach(() => {
+    wrapper = shallow(<App />);
+});
 
 it('Show a comment box', () => {
-    const wrapper = shallow(<App />);
+    // const wrapper = shallow(<App />);
     expect(wrapper.find(CommentBox).length).toEqual(1);
 });
 
-it('Show a Comment List ', () => {
-    const wrapper = shallow(<App />);
+it('Show a comment List', () => {
+    // const wrapper = shallow(<App />);
     expect(wrapper.find(CommentList).length).toEqual(1);
 });
