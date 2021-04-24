a4112058e51ec1f14f950e9a8df5f9b8f71e4f8b
pawel.siwek <pawel.siwek@filament.uk.com>
2021-04-24 22:30:51 +0100

    added form

--- src/components/commentBox.jsx
+++ src/components/commentBox.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
 
 export default class CommentBox extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            comment: '',
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ comment: e.target.value });
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        this.setState({
+           comment:'',
+       })
+    }
+    // Call an action creator 
+    // And save comment
+
     render() {
         return (
-            <div>
-             <h1>Comment Box</h1>
-            </div>
-        )
+            <form onSubmit={this.onSubmit}>
+                <h4>Add a Comment</h4>
+                <textarea onChange={this.onChange} value={this.state.comment}></textarea>
+                <div>
+                    <button >Submit Comment</button>
+                </div>
+            </form>
+        );
     }
 }
