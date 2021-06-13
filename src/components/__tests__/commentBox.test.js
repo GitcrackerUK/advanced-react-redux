4314b793bbe83f6b6652790df0f7949d6f4a42e0
pawel.siwek <pawel.siwek@filament.uk.com>
2021-06-13 20:13:58 +0100

    added mount and unmounnt + tests for div & form

--- src/components/__tests__/commentBox.test.js
+++ src/components/__tests__/commentBox.test.js
@@ -1,12 +1,20 @@
-import CommentBox from 'components/commentBox'
-import { mount } from 'enzyme'
-import React from 'react'
+import CommentBox from 'components/commentBox';
+import { mount } from 'enzyme';
+import React from 'react';
 
+let wrapped;
 
+beforeEach(() => {
+    wrapped = mount(<CommentBox />);
+});
+afterEach(() => {
+    wrapped.unmount(<CommentBox />);
+});
 it('Check has a text and a button', () => {
     const wrapper = mount(<CommentBox></CommentBox>);
 
+    expect(wrapper.find('div').length).toEqual(1)
+    expect(wrapper.find('form').length).toEqual(1);
     expect(wrapper.find('textarea').length).toEqual(1);
     expect(wrapper.find('button').length).toEqual(1);
 });
-
