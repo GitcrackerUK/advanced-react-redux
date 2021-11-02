1cc4e2cf08028014ab9b08c56f0999d2fe330496
Pawel <pawel.siwek@filament.uk.com>
2021-11-02 08:29:35 +0000

    added action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
-export const CREATE_COMMENT = 'CREATE_COMMENT' ;
+import { SAVE_COMMENT, ADD_USER } from 'actions/types'
+
+export function saveComment(comment){
+   return{
+     type : SAVE_COMMENT,
+     payload : comment
+   }
+}
+export function addUser(user){
+  return {
+    type : ADD_USER,
+    payload : user
+  }
+}
