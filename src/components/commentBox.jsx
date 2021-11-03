6b86b26cff8dfb7cc00af52d6604d3709b118680
Pawel <pawel.siwek@filament.uk.com>
2021-11-03 08:15:34 +0000

    addition logic for comentBox

--- src/components/commentBox.jsx
+++ src/components/commentBox.jsx
@@ -1,36 +1,79 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { saveComment, saveUser } from 'actions';
 
 export default class CommentBox extends Component {
     constructor(props) {
-        super();
+        super(props);
         this.state = {
             comment: '',
+            user: '',
+            userCreator: true ,
         };
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+        
+        this.commentChange = this.commentChange.bind(this);
+        this.userChange = this.userChange.bind(this);
+        this.addComment = this.addComment.bind(this);
+        this.addUser = this.addUser.bind(this);
+        this.toggle = this.toggle.bind(this);
     }
 
-    onChange(e) {
+    commentChange(e) {
         this.setState({ comment: e.target.value });
     }
-    onSubmit(e) {
-        e.preventDefault();
-        this.setState({
-           comment:'',
-       })
+    userChange(e) {
+        this.setState({ user: e.target.value });
     }
-    // Call an action creator 
-    // And save comment
+
+    addComment(e) {
+        e.preventDefault();
+        saveComment(this.state.comment);
+        this.setState({comment:''})
+    }
+    addUser(e) {
+        e.preventDefault();
+        saveUser(this.state.user);
+        this.setState({user:''})
+    }
+    toggle() {
+        this.setState({ userCreator: !this.state.userCreator });
+    }
+    userField() {
+        return (
+            <div>
+                <h4>Add User</h4>
+                <textarea onChange={this.userChange} value={this.state.user}></textarea>
+            </div>
+        );
+    }
+    commentField() {
+        console.log(this.state);
+        return (
+            <div>
+                <h4>Add a Comment</h4>
+                <textarea onChange={this.commentChange} value={this.state.comment}></textarea>
+            </div>
+        );
+    }
 
     render() {
         return (
-            <form onSubmit={this.onSubmit}>
-                <h4>Add a Comment</h4>
-                <textarea onChange={this.onChange} value={this.state.comment}></textarea>
-                <div>
-                    <button >Submit Comment</button>
-                </div>
-            </form>
+            <div>
+                <button style={{ display: 'block' }} onClick={this.toggle}>
+                    Change Input
+                </button>
+                <form onSubmit={this.state.userCreator ? this.addUser : this.addComment }>
+                    {this.state.userCreator ? this.userField() : this.commentField()}
+                    <div>
+                        <button>Submit</button>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
+function mapStateToProps(state) {
+    const { user, comment } = state
+    return { user, comment }
+  }
+connect(mapStateToProps, saveComment, saveUser)(CommentBox);
